9c8c7a8596b96cd96f21262f3d4c56e2b6ed0bf4
Santosh Kumar Bachar <santoshbachar@gmail.com>
2025-11-10 08:03:00 +0530

    feat: add method to run a stage

--- gocd/api/stage.py
+++ gocd/api/stage.py
@@ -6,10 +6,14 @@ __all__ = ['Stage']
 class Stage(Endpoint):
     base_path = 'go/api/stages/{id}'
 
-    def __init__(self, server, pipeline_name, stage_name, pipeline_counter=None):
+    def __init__(self, server, pipeline_name, stage_name, pipeline_counter=None, stage_counter=None):
         """A wrapper for the `Go stage API`__
 
         .. __: http://api.go.cd/current/#stages
+        details: only 1 API - to run a stage, stage_counter is not used
+
+        .. __: http://api.go.cd/current/#stage-instances
+        details: 5 APIs, stage_counter is used in 4/5 of them
 
         Args:
           server (Server): A configured instance of
@@ -21,10 +25,44 @@ class Stage(Endpoint):
         self.pipeline_name = pipeline_name
         self.pipeline_counter = pipeline_counter
         self.stage_name = stage_name
+        self.stage_counter = stage_counter
+        self.assist_in_getting_id = True
 
     def get_id(self):
-        return "{pipeline}/{stage}".format(pipeline=self.pipeline_name,
-                                           stage=self.stage_name)
+        if not self.assist_in_getting_id:
+            return ""
+
+        if self.pipeline_name is None or self.pipeline_counter is None or self.stage_name is None:
+            raise Exception('You must provide a pipeline name, a pipeline counter, and a stage '
+                            'name')
+
+        return "{pipeline_name}/{pipeline_counter}/{stage_name}".format(
+            pipeline_name=self.pipeline_name,
+            pipeline_counter=self.pipeline_counter,
+            stage_name=self.stage_name
+        )
+
+    def run(self):
+        """Runs a specified stage
+
+        See the `Go stage run documentation`__ for example responses.
+
+        .. __: https://api.gocd.org/current/#run-stage
+
+        Args:
+          pipeline_name (str): Name of the pipeline.
+          pipeline_counter (int): Name of the pipeline.
+          stage_name (str): Name of the pipeline.
+
+        Returns:
+          Response: :class:`gocd.api.response.Response` object
+        """
+
+        return self._post('/run', 202, headers={
+            "X-GoCD-Confirm": "true",
+            "Accept": "application/vnd.go.cd.v2+json"},
+            method="POST"
+        )
 
     def cancel(self):
         """Cancels a currently running stage
