2bfa6065fd755f0deb79e8603a8717afd793f738
Santosh Kumar Bachar <santoshbachar@gmail.com>
2026-05-10 13:33:32 +0530

    refactor: be compliant with Ram's system
    Signed-off-by: Santosh Kumar Bachar <santoshbachar@gmail.com>

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.12.12'
+        'Programming Language :: Python :: 3.6'
     ],
-    python_requires='==3.12.12'
+    python_requires='>=3.6'
 )
